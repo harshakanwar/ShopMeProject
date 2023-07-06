1a73116a066feab774feee8ec7bb62e64d9f9134
Harsha <harshakanwar12@gmail.com>
2023-07-06 18:00:08 +0530

    Initial Commit

---
+++ ShopMeWebParent/ShopMeBackend/src/main/java/com/shopme/admin/ShopMeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shopme.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopMeBackendApplication.class, args);
+	}
+
+}
